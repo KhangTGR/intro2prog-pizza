acad600e8a2910ea8b330327be0671c748acf42d
JustAbusiness <huysanti123456@gmail.com>
2022-12-30 17:53:00 +0700

    Add file

---
+++ src/View/Main.java
@@ -0,0 +1,26 @@
+package View;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myInt = 5;
+        double myDouble = 1.55;
+        String myText =  "Huy";
+
+        System.out.println((int) myInt);
+        System.out.println((double) myDouble);
+        System.out.println((String) myText);
+
+        }
+    }
+
+
+
+
+
+
+
+
