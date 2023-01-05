1ef13db6f1697beb43ad969fc81174e146da9440
4uroraL1ght <102708837+4uroraL1ght@users.noreply.github.com>
2023-01-05 16:59:26 +0700

    Update OrderController.java
    update

--- src/Controller/OrderController.java
+++ src/Controller/OrderController.java
@@ -1,5 +1,4 @@
-package src.Controller;
-
+package Src.Controller;
 import Model.OrderList;
 import utils.GenerateId;
 
@@ -12,20 +11,20 @@ public class OrderController {
     String customerID = readLogIn();
     String customerRank = readRank();
     public void addOrderCSV(OrderList orderList) {
-        GenerateId idMaker = new GenerateId();
+        GenerateId idCreator = new GenerateId();
         try {
             String orderPath = "src/Database/Order.csv";
-            long Id = idMaker.idIncrementor(orderPath);
-            FileWriter fw = new FileWriter(orderPath, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bw);
+            long Id = idCreator.idIncrementor(orderPath);
+            FileWriter filew = new FileWriter(orderPath, true);
+            BufferedWriter bw = new BufferedWriter(filew);
+            PrintWriter printw = new PrintWriter(bw);
             for(int i =0; i<(orderList.getOrders()).size();i++){
-                pw.println("ORD" + Id + ";" +orderList.getDate() + ";" + (((orderList.getOrders()).get(i)).getProducts()).getName() + ";" + (((orderList.getOrders()).get(i)).getQuantity())  + ";" + (((orderList.getOrders()).get(i)).getTotal()) + ";" + orderList.getTotal() +";" + customerID + ";" + (orderList.getCustomer()).getFullName() + ";" + (orderList.getCustomer()).getPhoneNum() + ";" + "Delivered");
+                printw.println("ORD" + Id + ";" +orderList.getDate() + ";" + (((orderList.getOrders()).get(i)).getProduct()).getName() + ";" + (((orderList.getOrders()).get(i)).getQuantity())  + ";" + (((orderList.getOrders()).get(i)).getTotal()) + ";" + orderList.getTotal() +";" + customerID + ";" + (orderList.getCustomer()).getFullName() + ";" + (orderList.getCustomer()).getPhoneNum() + ";" + "Delivered");
             }
-            pw.flush();
-            pw.close();
+            printw.flush();
+            printw.close();
         } catch (Exception E) {
-            System.out.println("There might be some problem... Failed");
+            System.out.println("It might be some problem, running failed");
         }
     }
 
@@ -33,12 +32,12 @@ public class OrderController {
     public String readLogIn(){
         String customerID = null;
         DataController dc = new DataController();
-        ArrayList<String> usernameList = dc.readData("src/Database/LoginUser.csv", 0);
+        ArrayList<String> usernameList = dc.readData("src/DataBase/LoginUser.csv", 0);
         String username = usernameList.get(0);
-        ArrayList<String> customerIdList = dc.readData("src/Database/CustomerRegister.csv", 0);
-        ArrayList<String> customerUserNameList = dc.readData("src/Database/CustomerRegister.csv", 3);
+        ArrayList<String> customerIdList = dc.readData("src/DataBase/CustomerRegister.csv", 0);
+        ArrayList<String> customerUserNameList = dc.readData("src/DataBase/CustomerRegister.csv", 3);
         GenerateId lineCount = new GenerateId();
-        for(int i =0; i<lineCount.idIncrementor("src/Database/CustomerRegister.csv"); i++){
+        for(int i =0; i<lineCount.idIncrementor("src/DataBase/CustomerRegister.csv"); i++){
             if(customerUserNameList.get(i).equals(username)){
                 customerID = customerIdList.get(i);
             }
@@ -49,12 +48,12 @@ public class OrderController {
     public String readRank(){
         String customerRank = null;
         DataController dc = new DataController();
-        ArrayList<String> usernameList = dc.readData("src/Database/LoginUser.csv", 0);
+        ArrayList<String> usernameList = dc.readData("src/DataBase/LoginUser.csv", 0);
         String username = usernameList.get(0);
-        ArrayList<String> customerRankList = dc.readData("src/Database/CustomerRegister.csv", 5);
-        ArrayList<String> customerUserNameList = dc.readData("src/Database/CustomerRegister.csv", 3);
+        ArrayList<String> customerRankList = dc.readData("src/DataBase/CustomerRegister.csv", 5);
+        ArrayList<String> customerUserNameList = dc.readData("src/DataBase/CustomerRegister.csv", 3);
         GenerateId lineCount = new GenerateId();
-        for(int i =0; i<lineCount.idIncrementor("src/Database/CustomerRegister.csv"); i++){
+        for(int i =0; i<lineCount.idIncrementor("src/DataBase/CustomerRegister.csv"); i++){
             if(customerUserNameList.get(i).equals(username)){
                 customerRank = customerRankList.get(i);
             }
@@ -62,27 +61,27 @@ public class OrderController {
         return customerRank;
     }
 
-    public double getTotalExpense(){
-        double totalExpense = 0;
+    public double getTotalUse(){
+        double totalUse = 0;
         GenerateId lineCount = new GenerateId();
         DataController dc = new DataController();
-        ArrayList<String> customerIdList = dc.readData("src/Database/Order.csv",6);
-        ArrayList<String> customerExpense = dc.readData("src/Database/Order.csv",5);
-        ArrayList<String> customerOrderId = dc.readData("src/Database/Order.csv",0);
+        ArrayList<String> customerIdList = dc.readData("src/DataBase/Order.csv",6);
+        ArrayList<String> customerExpense = dc.readData("src/DataBase/Order.csv",5);
+        ArrayList<String> customerOrderId = dc.readData("src/DataBase/Order.csv",0);
         String customerId = readLogIn();
-        for (int i = 0; i < lineCount.idIncrementor("src/Database/Order.csv"); i++) {
+        for (int i = 0; i < lineCount.idIncrementor("src/DataBase/Order.csv"); i++) {
             if(customerIdList.get(i).equals(customerId)){
-                if(i == lineCount.idIncrementor("src/Database/Order.csv") -1){
-                    totalExpense += Double.parseDouble(customerExpense.get(i));
+                if(i == lineCount.idIncrementor("src/DataBase/Order.csv") -1){
+                    totalUse += Double.parseDouble(customerExpense.get(i));
                     break;
                 }
                 if(customerOrderId.get(i).equals(customerOrderId.get(i+1))){
                     continue;
                 }
-                totalExpense += Double.parseDouble(customerExpense.get(i));
+                totalUse += Double.parseDouble(customerExpense.get(i));
             }
         }
-        return totalExpense;
+        return totalUse;
     }
 
 }
